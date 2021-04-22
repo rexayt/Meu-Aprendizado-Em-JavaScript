3fd9b77131ed2a3a820e48e35c7137feb6697d9f
rexayt <69477649+rexayt@users.noreply.github.com>
2021-04-22 15:30:32 -0300

    Começando
    Eu estou começando aos poucos nesse curso, vocês podem esperar mais atualizações futuramentes.

---
+++ Seção 3 - Fundamentos/1 - comentário.js
@@ -0,0 +1,23 @@
+//Comentário de uma linha (associado à linha próxima)
+console.log('Linha 1')
+
+/*
+comentário de
+múltiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+*Comentário de 
+*múltiplas linhas
+*
+*
+*
+*/
+console.log('Linha 3')
+
+/*
+*Evitar comentários óbvios
+*Prefirir código claro/ limpo
+*Eu posso comentar tudo que eu quiser, para internalizar o que foi visto nas aulas
+*/
---
+++ Seção 3 - Fundamentos/3 - tipagem fraca.js
@@ -0,0 +1,8 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//typeof mostra o tipo do valor da variável ou constante
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
