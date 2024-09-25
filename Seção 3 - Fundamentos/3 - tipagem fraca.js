88996344de1fc31c593742c6f815ad7d2a90a880
Luiz <luizroberto3268@gmail.com>
2024-09-25 15:12:20 -0300

    New Commit

--- Seção 3 - Fundamentos/3 - tipagem fraca.js
+++
@@ -1,8 +0,0 @@
-let qualquer = 'Legal'
-console.log(qualquer)
-console.log(typeof qualquer)
-
-//typeof mostra o tipo do valor da variável ou constante
-qualquer = 3.1516
-console.log(qualquer)
-console.log(typeof qualquer)
---
+++ fundamentos/Números alguns cuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0)
+//o JavaScript converte automáticamente strings de números em números.
+console.log("10"/ 2) //só funciona se tiver apenas os números
+console.log("Show!" * 2)
+console.log(0.1 + 07)
+console.log((10.345).toFixed(2)) //roFixed(2) transforma em string
